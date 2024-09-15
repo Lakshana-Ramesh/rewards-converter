8a526cb8686c17b9699154beee254ea3466276d6
Lakshana-Ramesh <misslakshanaramesh@gmail.com>
2024-09-16 02:11:05 +0530

    Update RewardValueTests.java

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -19,16 +19,30 @@ public class RewardValueTests {
     }
 
     @Test
-    void convert_from_cash_to_miles() {
-        double cashValue = 100.0;
-        var rewardValue = new RewardValue(cashValue);
-        assertEquals(10000, rewardValue.getMilesValue());
-    }
+   void convert_from_cash_to_miles() {
+       assert false;
+       double cashValue = 100.0;
+       var rewardValue = new RewardValue(cashValue);
+       assertEquals(10000, rewardValue.getMilesValue());
+   }
 
-    @Test
-    void convert_from_miles_to_cash() {
-        int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
-        assertEquals(100.0, rewardValue.getCashValue(), 0.0001); // added a delta value of 0.0001
-    }
+
+   @Test
+   void convert_from_miles_to_cash() {
+       assert false;
+       int milesValue = 10000;
+       var rewardValue = new RewardValue(milesValue);
+       assertEquals(100.0, rewardValue.getCashValue(), 0.0001); // added a delta value of 0.0001
+   }
 }
+}
+
+
+
+
+
+
+
+
+
+  
