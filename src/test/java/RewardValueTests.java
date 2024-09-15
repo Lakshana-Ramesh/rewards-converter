bf839e225b7befd5701ecc6e6bebef643ec07f34
Lakshana-Ramesh <misslakshanaramesh@gmail.com>
2024-09-16 02:16:39 +0530

    Update RewardValueTests.java

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -2,47 +2,39 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class RewardValueTests {
-
+public class RewardValueTests 
+{
     @Test
-    void create_with_cash_value() {
+    void create_with_cash_value() 
+    {
         double cashValue = 100;
-        var rewardValue = new RewardValue(cashValue);
+        int rewardValue = new RewardValue(cashValue);
         assertEquals(cashValue, rewardValue.getCashValue());
     }
 
     @Test
-    void create_with_miles_value() {
+    void create_with_miles_value() 
+    {
         int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
+        int rewardValue = new RewardValue(milesValue);
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
-   void convert_from_cash_to_miles() {
-       assert false;
-       double cashValue = 100.0;
-       var rewardValue = new RewardValue(cashValue);
-       assertEquals(10000, rewardValue.getMilesValue());
-   }
+    void convert_from_cash_to_miles() 
+    {
+        double cash = 97777;
+        int miles = (int) (cash / 0.0035);
+        int rewardValue = new RewardValue(cash);
+        assertEquals(miles, rewardValue.getMilesValue());
+    }
 
-
-   @Test
-   void convert_from_miles_to_cash() {
-       assert false;
-       int milesValue = 10000;
-       var rewardValue = new RewardValue(milesValue);
-       assertEquals(100.0, rewardValue.getCashValue(), 0.0001); // added a delta value of 0.0001
-   }
+    @Test
+    void convert_from_miles_to_cash() 
+    {
+        int miles = 98777;
+        double expectedCashValue = miles * 0.0035;
+        int rewardValue = new RewardValue(miles);
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
+    }
 }
-}
-
-
-
-
-
-
-
-
-
-  
